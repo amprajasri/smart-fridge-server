6b5794112b75c2f85679b1eda0cf7888084f3f08
Amprajari <amprajasri@gmail.com>
2025-01-01 17:11:31 +0530

    solved pool error

--- db.js
+++ db.js
@@ -4,5 +4,4 @@ require('dotenv').config()
 const pool = new Pool({
   connectionString: "postgres://default:VUKM7cdPseS6@ep-shy-sky-a4maxx4l-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
 })
-
-export default pool
+module.exports = pool;
