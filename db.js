efcddc28e7ac545f43a62a95b201a492f901d0ed
Amprajari <amprajasri@gmail.com>
2025-01-01 15:30:34 +0530

    added db

---
+++ db.js
@@ -0,0 +1,6 @@
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const pool = new Pool({
+  connectionString: "postgres://default:VUKM7cdPseS6@ep-shy-sky-a4maxx4l-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
+})
