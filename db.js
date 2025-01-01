c23e2cfc67ce59b56bdb88fec0862acf1bcd70a8
Amprajari <amprajasri@gmail.com>
2025-01-01 17:13:50 +0530

    pool error 2

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg')
-require('dotenv').config()
+// require('dotenv').config()
 
 const pool = new Pool({
   connectionString: "postgres://default:VUKM7cdPseS6@ep-shy-sky-a4maxx4l-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
